78a09c12bf2eb15aa84d3eb9cb9b8b20b775244a
ChantalSchot <chantalschot@outlook.com>
2020-03-02 11:15:45 +0100

    initial commit

---
+++ helloworld.ts
@@ -0,0 +1,7 @@
+let message: string = 'Hello World!';
+if (false) {
+	message = "never happen";
+}
+console.log(message);
+var testArray = [1, "hoi", 3];
+
---
+++ out/helloworld.js
@@ -0,0 +1,7 @@
+var message = 'Hello World!';
+if (false) {
+    message = "never happen";
+}
+console.log(message);
+var testArray = [1, "hoi", 3];
+//# sourceMappingURL=helloworld.js.map
